accb9460e2cb19ea35d7326af278983cbba171f2
Nathaniel Burbank <nathaniel.burbank@gmail.com>
2016-09-30 14:55:21 -0400

    cleaned up tests

--- binsearch_test.py
+++ binsearch_test.py
@@ -2,17 +2,29 @@ from pytest import raises
 from binsearch import binary_search
 import numpy as np
 
-input = list(range(10))
+input = list(range(100))
 
-def provided_examples_one():
+def test_one():
+	#Basic binary search operations 
 	assert binary_search(input, 5) == 5
-	assert binary_search(input, 4.5) == -1
-	assert binary_search(input, 10) == -1
+	assert binary_search(input, 75) == 75
+	assert binary_search(input, 9.5) == -1
+	assert binary_search(input, 100) == -1
 	assert binary_search([5], 5) == 0
 	assert binary_search([5], 4) == -1 
 
-def provided_examples_one():
+def tests_two():
+	#with Numpy
 	assert binary_search([1,2,np.inf], 2) == 1
 	assert binary_search([1,2,np.inf], np.inf) == 2
 	assert binary_search(input, 5, 1,3) == -1
 	assert binary_search(input, 2, 2, 2) == 2
+
+def test_input_type():
+	#error conditions
+	with raises(TypeError):
+		binary_search(['Chars',5])
+
+def test_blank_array():
+	with raises(TypeError):
+		binary_search([[],3])
