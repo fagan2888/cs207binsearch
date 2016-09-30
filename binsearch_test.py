bb75492f4a3e6cadcfc21eaf03287fb6418580fe
Nathaniel Burbank <nathaniel.burbank@gmail.com>
2016-09-30 14:44:39 -0400

    added requirments.txt

--- binsearch_test.py
+++ binsearch_test.py
@@ -30,12 +30,16 @@ import numpy as np
 
 input = list(range(10))
 
-def provided_examples():
+def provided_examples_one():
 	assert binary_search(input, 5) == 5
 	assert binary_search(input, 4.5) == -1
 	assert binary_search(input, 10) == -1
 	assert binary_search([5], 5) == 0
 	assert binary_search([5], 4) == -1 
+
+def provided_examples_one():
+	assert binary_search([1,2,np.inf], 2) == 1
+	assert binary_search([1,2,np.inf], np.inf) == 2
 
 
 	
